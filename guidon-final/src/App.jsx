832ae982cebaa994a7eca7f912829d208d9660a4
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-07-08 11:14:58 +0800

    addded mobile menu toggle

--- guidon-final/src/App.jsx
+++ guidon-final/src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+
 const App = () => {
+  const [activeIcon, setActiveIcon] = useState("hamburger");
+
+  const toggleIcon = () => {
+    setActiveIcon((prevIcon) =>
+      prevIcon === "hamburger" ? "close" : "hamburger"
+    );
+  };
+
   return (
     <div>
-      <Header />
-      <Nav />
+      <Header activeIcon={activeIcon} toggleIcon={toggleIcon} />
+      <Nav activeIcon={activeIcon} toggleIcon={toggleIcon} />
     </div>
   );
 };
