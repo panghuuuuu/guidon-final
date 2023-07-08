4bc863bb0df764f4f1da280440757be5b4e21975
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-07-08 22:03:22 +0800

    created index-photo section

--- guidon-final/src/App.jsx
+++ guidon-final/src/App.jsx
@@ -6,6 +6,7 @@ import Recents from "./components/Recents";
 import Topics from "./components/Topics";
 import Breaking from "./components/Breaking";
 import UAAP from "./components/UAAP";
+import IndexPhoto from "./components/IndexPhoto";
 
 const App = () => {
   const [activeIcon, setActiveIcon] = useState("hamburger");
@@ -25,6 +26,7 @@ const App = () => {
       <Topics />
       <Breaking />
       <UAAP />
+      <IndexPhoto />
     </div>
   );
 };
---
+++ guidon-final/src/components/IndexPhoto.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../stylesheets/index-photo.css";
+const IndexPhoto = () => {
+  return (
+    <section id="index-photo">
+      <a
+        id="index-photos"
+        target="_blank"
+        class="article-wrapper"
+        href="https://tgdn.co/3bZaoHc"
+        rel="norefferer noreferrer"
+      >
+        <div class="background-wrapper">
+          <div class="main-wrapper">
+            <h2 class="title">
+              In welcoming display, LS community greets 2600 freshmen in OrSem
+              Tindig
+            </h2>
+            <div class="excerpt">
+              In view of the upcoming semester’s lighter protocols, OrSem Tindig
+              was celebrated through a hybrid setup, with online activities held
+              from August 3 to 6 and onsite events from August 9 to 10.
+            </div>
+            <h6 class="indiv-date photo-date">August 13, 2022</h6>
+          </div>
+        </div>
+      </a>
+    </section>
+  );
+};
+
+export default IndexPhoto;
